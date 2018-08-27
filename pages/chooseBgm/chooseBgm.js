5d38c78fb1ef3fee5e2157285ead32e2e4d8948a
cumtQt <1390927050@qq.com>
2018-08-27 19:06:20 +0800

    的

--- pages/chooseBgm/chooseBgm.js
+++ pages/chooseBgm/chooseBgm.js
@@ -19,16 +19,14 @@ Page({
         title: '请等待...',
       });
       var serverUrl = app.serverUrl;
-      var user = app.getGlobalUserInfo();
-      debugger;
+       
+       
       // 调用后端
       wx.request({
-        url: serverUrl + '/bgm/list',
+        url: serverUrl + 'video/bgmList',
         method: "POST",
         header: {
           'content-type': 'application/json', // 默认值
-          'headerUserId': user.id,
-          'headerUserToken': user.userToken
         },
         success: function (res) {
           console.log(res.data);
